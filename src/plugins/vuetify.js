41b25db10e7a3aea0329dffe762dd82ba31b9732
SamuelQuetin <samquetinuel@gmail.com>
2025-03-24 14:38:22 +0100

    Ajout Vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,70 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Composables
+import {createVuetify} from 'vuetify'
+import {en, fr} from 'vuetify/locale'
+
+//CUstom
+const itemLightTheme = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    'surface-bright': '#FFFFFF',
+    'surface-light': '#EEEEEE',
+    'surface-variant': '#424242',
+    'on-surface-variant': '#EEEEEE',
+    primary: '#295494',
+    'primary-darken-1': '#1F5592',
+    secondary: '#252C61',
+    'secondary-darken-1': '#018786',
+    textColor: '#FFFFFF',
+    error: '#a32525',
+    info: '#326bb1',
+    success: '#4da432',
+    warning: '#ec6839',
+  },
+  variables: {
+    'border-color': '#000000',
+    'border-opacity': 0.12,
+    'high-emphasis-opacity': 0.87,
+    'medium-emphasis-opacity': 0.60,
+    'disabled-opacity': 0.38,
+    'idle-opacity': 0.04,
+    'hover-opacity': 0.04,
+    'focus-opacity': 0.12,
+    'selected-opacity': 0.08,
+    'activated-opacity': 0.12,
+    'pressed-opacity': 0.12,
+    'dragged-opacity': 0.08,
+    'theme-kbd': '#212529',
+    'theme-on-kbd': '#FFFFFF',
+    'theme-code': '#F5F5F5',
+    'theme-on-code': '#000000',
+  }
+}
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  locale: {
+    locale: 'fr',
+    fallback: 'en',
+    messages: { fr, en },
+  },
+  theme: {
+    defaultTheme: 'itemLightTheme',
+    themes: {
+      itemLightTheme,
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',
+  }
+})
