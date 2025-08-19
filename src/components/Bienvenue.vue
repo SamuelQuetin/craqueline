88ce4167fd52147f2f014ce74865571155679f60
SamuelQuetin <quetin@abes.fr>
2025-08-19 17:03:57 +0200

    fix cols

--- src/components/Bienvenue.vue
+++ src/components/Bienvenue.vue
@@ -2,14 +2,14 @@
     <h2 class="pb-2">Bienvenue</h2>
     <p class="py-2 font-italic"> « S’il fallait préciser quand l’idée de Craqueline a germé dans nos esprits, nous dirions que ce fut dès l’enfance. Mamie mettait toujours un point d’honneur à terminer ses repas de fêtes par un Saint–Honoré. Le feuilletage croustillant, les choux moelleux au caramel craquant et la crème délicatement parfumée à la vanille… C’est notre madeleine de Proust à nous. C’est l’essence même de Craqueline : le désir qu’une bouchée suffise à retomber en enfance. »</p>
     <v-row cols="12" class="mb-4">
-      <v-col cols="8" xs="12" sm="12" md="12" lg="8" xl="8">
+      <v-col cols="12" xs="12" sm="12" md="12" lg="8" xl="8">
         <p> Bertille et Mélissa, ce sont deux cousines liées par leur grand-mère Jacqueline. Cuisinière et pâtissière passionnée, elle sera une véritable source d’inspiration pour Bertille qui décide de se former en cuisine, puis en pâtisserie et en chocolaterie.
           10 plus tard, riche de son expérience – tantôt en pâtisserie de boutique, tantôt en restaurant gastronomique – elle quitte son emploi avant que Mélissa ne la rejoigne avec enthousiasme dans cette aventure.
         </p>
         <br/>
         <p> C’est ainsi qu’elles créent Craqueline, nommé ainsi d’après la contraction de craquelin (nom du biscuit que l’on dépose sur la pâte à chou avant de la cuire) et de Jacqueline.</p>
       </v-col>
-      <v-col cols="4" xs="12" sm="12" md="12" lg="4" xl="4" class="d-flex justify-center">
+      <v-col cols="12" xs="12" sm="12" md="12" lg="4" xl="4" class="d-flex justify-center">
         <v-img src="@/assets/photo/SelfiBertMel.png" max-width="30em"></v-img>
       </v-col>
     </v-row>
