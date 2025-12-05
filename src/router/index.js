52ac08fec28b9b9e1094bdfc34e26260c1df5fc1
Julien FAURE <julien.faure@iaca-electronique.com>
2025-12-05 11:47:18 +0100

    Add custom scrollBehavior to router for smooth scrolling and hash navigation

--- src/router/index.js
+++ src/router/index.js
@@ -27,9 +27,24 @@ const routes = [
     },
 ]
 
+
+
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior: (to, from, savedPosition) => {
+        if (savedPosition) {
+            return savedPosition;
+        }
+        if (to.hash) {
+            return {
+                el: to.hash,
+                top: 180,
+                behavior: 'smooth'
+            };
+        }
+        return { left: 0, top: 0 };
+    },
 })
 
 export default router
